e6c8757c1834069b983f32ed8a1c9441cea6648d
jadhavarun602-hub <jadhavarun602@gmail.com>
2025-11-20 19:50:37 +0530

    if_else statement

---
+++ if_else_1.js
@@ -0,0 +1,11 @@
+let num = 7;
+
+if (num < 0) {
+    console.log("This is the negative number");
+}else if(num == 0){
+    console.log("This is the zero number");
+}else if(num > 0){
+    console.log("This is the positive number");
+}else{
+    console.log("this is the not a number");
+}
