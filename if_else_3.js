2ba7aa20ad9830a5dac0cad8fe90ac8b26dec965
jadhavarun602-hub <jadhavarun602@gmail.com>
2025-11-20 20:07:42 +0530

    if_else_Q3

---
+++ if_else_3.js
@@ -0,0 +1,9 @@
+// Take a user's age and check if the user is eligible to vote (age ≥ 18).
+
+let age = 24;
+
+if (age >= 18) {
+    console.log("you are eligible, you can vote");
+} else {
+    console.log("you are not eligible, you can't vote");
+}
