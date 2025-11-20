3eb91aa077fd6fa20082319d73f898ed0e32bb8d
jadhavarun602-hub <jadhavarun602@gmail.com>
2025-11-20 20:28:46 +0530

    if_else_Q4

---
+++ if_else_4.js
@@ -0,0 +1,9 @@
+// Check whether a given year is a leap year.
+
+let year = 2028;
+
+if (year % 4 == 0) {
+    console.log("this is the leap year");
+} else {
+    console.log("this is not leap year");
+}
