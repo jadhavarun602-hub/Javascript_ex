4eba89c8a13189006b31a6a537edfa46557620e8
jadhavarun602-hub <jadhavarun602@gmail.com>
2025-11-21 20:34:51 +0530

    if_else_10

---
+++ if_else_10.js
@@ -0,0 +1,9 @@
+// Check if a number is between 1 and 100.
+
+let num = 177;
+
+if (num <= 100 && num >= 1 ) {
+    console.log(num+" number is between 1 and 100");    
+} else {
+    console.log(num+" number is not between 1 and 100");
+}
