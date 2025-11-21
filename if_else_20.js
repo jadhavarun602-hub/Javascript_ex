01303def445fc3238b1b3dfc1035cd8b5f9a132f
jadhavarun602-hub <jadhavarun602@gmail.com>
2025-11-21 23:18:05 +0530

    if_else_Q20

---
+++ if_else_20.js
@@ -0,0 +1,12 @@
+// Check if the number is positive, negative or zero.
+
+let num = 33;
+
+if (num < 0) {
+    console.log(num+" is a negative number"); 
+} else if (num === 0) {
+    console.log(num+" is a zero number ");
+} else {
+    console.log(num+" is a positive number");
+    
+} 
