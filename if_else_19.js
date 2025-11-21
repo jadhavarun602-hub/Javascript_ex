586a96ceca49a5d2ad36113e027ce005074d4c6f
jadhavarun602-hub <jadhavarun602@gmail.com>
2025-11-21 23:11:54 +0530

    if_else_Q19

---
+++ if_else_19.js
@@ -0,0 +1,9 @@
+// Check if a person can apply for a driving license (age ≥ 18).
+
+const age = 27;
+
+if (age >= 18) {
+    console.log("you can apply for a driving license");  
+} else {
+    console.log("you can't apply for a driving license");  
+}
