1a29bd1f9c56bbaea737b2493dcff4bf3487fb86
jadhavarun602-hub <jadhavarun602@gmail.com>
2025-11-22 00:02:23 +0530

    if_else_Q23

---
+++ if_else_23.js
@@ -0,0 +1,12 @@
+// Check whether two numbers are equal or not.
+
+let num1 = 44;
+let num2 = 4;
+
+if (num1 === num2) {
+    console.log("two numbers are equal");
+    
+} else {
+    console.log("two numbers are not equal");
+    
+}
