cb63a3d4725d579c07a27f43e18bd53e84d8aba1
jadhavarun602-hub <jadhavarun602@gmail.com>
2025-11-21 22:39:32 +0530

    if_else_Q16

---
+++ if_else_16.js
@@ -0,0 +1,12 @@
+// Check if a number is a perfect square
+
+let num = 20; 
+
+let root = Math.sqrt(num);
+
+if (root % 1 === 0) {
+    console.log(num + " is a Perfect Square");
+} else {
+    console.log(num + " is NOT a Perfect Square");
+}
+
