ca49271407cd93098322c4ad8528f545fc3e2db3
jadhavarun602-hub <jadhavarun602@gmail.com>
2025-11-21 22:13:19 +0530

    if_else_Q14

---
+++ if_else_14.js
@@ -0,0 +1,10 @@
+// Check if a number is a multiple of 3 or 7.
+
+let num = 14;
+
+if (num % 3 === 0 || num % 7 === 0) {
+    console.log(num+" number is a multiple of 3 or 7.");  
+} else {
+    console.log(num+" number is a not multiple of 3 or 7.");
+    
+}
