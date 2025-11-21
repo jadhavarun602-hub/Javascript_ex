1bca6bb33fff765165be612d39a30c33082aaccc
jadhavarun602-hub <jadhavarun602@gmail.com>
2025-11-21 21:18:18 +0530

    if_else_Q13

---
+++ if_else_13.js
@@ -0,0 +1,13 @@
+// Check if a username and password match stored values.
+
+let stored_username = "arun";
+let stored_password = "arun123";
+
+let username = "arun";
+let password = "arun123";
+
+if (stored_username == username && stored_password == password) {
+    console.log("Login Success!");  
+} else {
+    console.log("Login Failed!");   
+}
