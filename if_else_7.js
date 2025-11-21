8ee858173b932f33c23893642982928bf3d7884e
jadhavarun602-hub <jadhavarun602@gmail.com>
2025-11-21 20:09:56 +0530

    if_else_Q7_Q8

---
+++ if_else_7.js
@@ -0,0 +1,10 @@
+// Check if a character is vowel or consonant.
+
+let vowel = "b";
+
+if (vowel == 'a' || vowel == 'i' || vowel == 'o' || vowel == 'u' || vowel == 'e' ||
+    vowel == 'A' || vowel == 'I' || vowel == 'O' || vowel == 'U' || vowel == 'E') {
+        console.log(vowel+" is a vowel character");
+} else {
+    console.log(vowel+" is consonant character"); 
+}
---
+++ if_else_8.js
@@ -0,0 +1,10 @@
+// Check whether a number is divisible by 5 and 11 or not.
+
+const num = 110;
+
+if (num % 5 == 0 && num % 11 == 0) {
+    console.log(num+ " this number is divisible by 5 and 11");
+} else {
+    console.log( num+" this number is not divisible by 5 and 11");
+    
+}
