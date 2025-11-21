d74e1cd9e0e55cd0b5142ecc6f2bd07359b30bae
jadhavarun602-hub <jadhavarun602@gmail.com>
2025-11-21 21:01:32 +0530

    if_else_Q12

---
+++ if_else_12.js
@@ -0,0 +1,12 @@
+// Check whether a person is child, teen, or adult.
+
+const person = 21;
+
+if (person <= 12) 
+{
+    console.log("This is the child Person!");
+} else if(person <= 19 && person >= 13){
+    console.log("This is the teenager Person!");   
+}else{
+    console.log("this is the adult Person!");  
+}
