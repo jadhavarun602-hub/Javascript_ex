ab1c94faf163dc2a1f271ab2ffeebb1240c020e1
jadhavarun602-hub <jadhavarun602@gmail.com>
2025-11-22 00:07:02 +0530

    if_else_Q24

---
+++ if_else_24.js
@@ -0,0 +1,12 @@
+// Check whether a string is empty or not.
+
+
+let str = "arun";
+
+if (str.length === 0) {
+    console.log("string is empty");
+    
+} else {
+    console.log("string is not empty");
+    
+}
