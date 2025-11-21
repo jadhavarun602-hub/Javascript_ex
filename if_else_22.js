b7b6ea8cf85011079cffd1006b9c0ca3e1a69dfd
jadhavarun602-hub <jadhavarun602@gmail.com>
2025-11-21 23:47:31 +0530

    if_else_Q22

---
+++ if_else_22.js
@@ -0,0 +1,12 @@
+// Check if the user entered correct OTP.
+
+let OTP = 565654;
+let otp = 565654;
+
+if (otp === OTP ) {
+    console.log("OTP is matched");
+    
+} else {
+    console.log("OTP is not matched , please try again!");
+    
+}
