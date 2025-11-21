bd9690116e7d376bbc5818b2e8e3fd3dc9b5cba4
jadhavarun602-hub <jadhavarun602@gmail.com>
2025-11-21 23:40:23 +0530

    if_else_Q21

---
+++ if_else_21.js
@@ -0,0 +1,44 @@
+// Take month number and print number of days.
+
+ let month = 5;
+
+ if (month === 1) {
+    console.log("January : number of day : 31");
+    
+ } else if (month === 2) {
+    console.log("February : number of day : 28 ( 29 in leap year )");
+    
+ } else if (month === 3) {
+    console.log("March : number of day : 31");
+
+ }else if(month === 4){
+    console.log("April : number of day : 30");  
+
+ }else if(month === 5){
+    console.log("May : number of day : 31");
+    
+ }else if(month === 6){
+    console.log("June : number of day : 30");
+    
+ }else if(month === 7){
+    console.log("July : number of day : 31");
+    
+ }else if(month === 8){
+    console.log("August : number of day : 31");
+    
+ }else if(month === 9){
+    console.log("September : number of day : 30");
+    
+ }else if(month === 10){
+    console.log("October : number of day : 31");
+    
+ }else if(month === 11){
+    console.log("November : number of day : 30");
+    
+ }else if(month === 12){
+    console.log("December : number of day : 31");
+    
+ }else{
+    console.log("Please enter valid month number.....");
+    
+ }
